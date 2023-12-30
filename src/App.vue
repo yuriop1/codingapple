1f9561353952a0ffeed7b7c7733f39cbeaea44b4
yuriop1 <yuriop1@naver.com>
2023-12-31 01:53:49 +0900

    [...]

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,35 @@
 <template>
-  <modalTop :원룸들 = "원룸들" :popupOpenCount="popupOpenCount" :모달창오픈여부="모달창오픈여부" @closePopup="모달창오픈여부 = false"></modalTop>
+  <Transition name = "fade">
+    <modalTop :원룸들 = "원룸들" :popupOpenCount="popupOpenCount" :모달창오픈여부="모달창오픈여부" @closePopup="모달창오픈여부 = false"></modalTop>
+  </Transition>
+
   <div>
     <div class="menu">
       <a v-for="keyname in menus" :key="keyname">{{ keyname }}</a>
     </div>
-    <DisCountRoom></DisCountRoom>
+
+    <DisCountRoom v-if="showDiscount === true"></DisCountRoom>
+
+    <button @click="priceSort">가격순정렬</button>
+    <button @click="sortBack">되돌리기</button>
+
     <Card :원룸 = "원룸들[i]" v-for="(room, i) in 원룸들" :key="room" @openModal="모달창오픈여부 = true; popupOpenCount = $event"></Card>
   </div>
-
-
 </template>
 
 <script>
-import oneRoomData from './assets/oneRoom.js';
+import data from './assets/oneRoom.js';
 import DisCountRoom from './DiscountRoom.vue';
 import ModalTop from './ModalTop.vue';
 import Card from './Card.vue';
 
 export default {
   name: 'App',
+  components:{
+    DisCountRoom : DisCountRoom,
+    ModalTop,
+    Card,
+  },
   data(){
     return{ 
       모달창오픈여부 : false,
@@ -28,14 +39,15 @@ export default {
       신고수3 : 0,
       menus : ['home', 'Shop', 'About'],
       products : ['역삼', '천호', '마포'],
-      원룸들 : oneRoomData,
+      원룸들 : data,
+      원룸들오리지널 : [...data],
+      showDiscount : true,
     }
   },
-  components:{
-    DisCountRoom : DisCountRoom,
-    ModalTop,
-    Card,
+
+  mounted(){
   },
+
   methods : {
     increase1(){
       this.신고수1 +=1;
@@ -45,6 +57,14 @@ export default {
     },
     increase3(){
       this.신고수3 +=1;
+    },
+    priceSort(){
+      this.원룸들.sort(function(a,b){
+        return a.price - b.price
+      })
+    },
+    sortBack(){
+      this.원룸들 = [...this.원룸들오리지널];
     },
   },
 }
@@ -92,5 +112,32 @@ div {
   padding: 10px;
   margin:10px;
   border-radius:5px;
+}
+.start {
+  opacity : 0;
+  transition : all 1s;
+}
+.end {
+  opacity : 1;
+}
+
+.fade-enter-from{
+  opacity: 0;
+}
+.fade-enter-active{
+  transition: all 1s;
+}
+.fade-enter-to{
+  opacity: 1;
+}
+
+.fade-leave-from{
+  opacity: 1;
+}
+.fade-leave-active{
+  transition: all 1s;
+}
+.fade-leave-to{
+  opacity: 0;
 }
 </style>
