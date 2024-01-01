49717288933512a85d785c9ee9feee5074d9a85d
yuriop1 <yuriop1@naver.com>
2024-01-02 02:03:27 +0900

    home 재생성

---
+++ home/src/DiscountRoom.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="discount">
+      <h4>지금결제하면 {{ sales }}%할인</h4>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'DiscountRoom',
+    data(){
+      return{ 
+        sales : 99,
+      }
+    },
+    mounted(){
+      setInterval(()=>{
+        this.sales =  this.sales -1;
+      }, 1000);
+    },
+  }
+  </script>
+  
+  <style>
+  
+  </style>
--- home/vue.config.js
+++ home/vue.config.js
@@ -1,4 +1,18 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+
+  transpileDependencies: true,
+
+  lintOnSave : false,
+
+  pluginOptions: {
+
+    vuetify: {
+
+      // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
+
+    }
+
+  }
+
 })
